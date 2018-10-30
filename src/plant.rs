68799c36256cc29305ce8f133793c0e1d310ebc9
Lucas Brendel <lucasbrendel@gmail.com>
2018-10-29 23:21:00 -0500

    formatting file

--- src/plant.rs
+++ src/plant.rs
@@ -2,18 +2,18 @@
 //! type of plant that could be tracked and maintained in the garden or orchard.
 //! This item should contain the necessary data so that work can be done for the use to alert
 //! when work is needed to be performed.
-struct Plant{
-    name: String
+struct Plant {
+    name: String,
 }
 
 #[cfg(test)]
-mod tests{
+mod tests {
     use super::*;
 
     #[test]
-    fn display_name(){
-        let plant = Plant{
-            name: String::from("Tomato")
+    fn display_name() {
+        let plant = Plant {
+            name: String::from("Tomato"),
         };
         assert_eq!("Tomato", plant.name);
     }

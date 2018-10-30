6918b1595d9697625a611b80c801ac5a460cbe9b
Lucas Brendel <lucasbrendel@gmail.com>
2018-10-29 22:15:14 -0500

    Adding clap dependency and add a default command line object

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+extern crate clap;
+
+use clap::{Arg, App, SubCommand};
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("Garden Master")
+        .version("0.1.0")
+        .author("Lucas Brendel")
+        .about("Help keep track of all the things that need to happen in a garden or orchard")
+        .get_matches();
 }

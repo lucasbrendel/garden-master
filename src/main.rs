5d028e24a9292830eb2142148b15f6a825d08cf5
Lucas Brendel <lucasbrendel@gmail.com>
2018-10-29 22:40:08 -0500

    Formated code and removed unused imports

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 extern crate clap;
 
-use clap::{Arg, App, SubCommand};
+use clap::{App};
 
 fn main() {
-    let matches = App::new("Garden Master")
+    let _matches = App::new("Garden Master")
         .version("0.1.0")
         .author("Lucas Brendel")
         .about("Help keep track of all the things that need to happen in a garden or orchard")

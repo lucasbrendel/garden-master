7e6d98886c7df2d9cd3db52140a03de97f51a920
Lucas Brendel <lucasbrendel@gmail.com>
2018-11-02 21:20:08 -0500

    Adding SQL Lite crate

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
 extern crate chrono;
 extern crate clap;
+extern crate rusqlite;
 
 use clap::App;
 
 mod crop;
 mod plant;
+
+use plant::{Plant, PlantType};
+use rusqlite::types::ToSql;
+use rusqlite::{Connection, NO_PARAMS};
 
 fn main() {
     let _matches = App::new("Garden Master")
@@ -12,4 +17,8 @@ fn main() {
         .author("Lucas Brendel")
         .about("Help keep track of all the things that need to happen in a garden or orchard")
         .get_matches();
+
+    let tomato = Plant::new(String::from("Tomato"), 50, 50);
+
+    let conn = Connection::open_in_memory().unwrap();
 }

48554eea4060c34e7e29c2425ffd575372949d97
Lucas Brendel <lucasbrendel@gmail.com>
2018-11-03 23:19:21 -0500

    adding a comment to logging

--- src/logging.rs
+++ src/logging.rs
@@ -1,6 +1,14 @@
 use simplelog::*;
 use std::fs::File;
 
+/// Basic abstraction for initializing logging.
+/// Currently using SimpleLog to log events.
+/// The following macros are available:logging_init()
+/// error!
+/// info!
+/// debug!
+/// warn!
+/// trace!
 pub fn logging_init() {
     CombinedLogger::init(vec![
         TermLogger::new(LevelFilter::Warn, Config::default()).unwrap(),

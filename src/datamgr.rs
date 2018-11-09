38b43c26dd815ed6647f094e20f4e591a12d869a
Lucas Brendel <lucasbrendel@gmail.com>
2018-11-08 23:33:38 -0600

    updating crop data access. not tested

--- src/datamgr.rs
+++ src/datamgr.rs
@@ -18,9 +18,16 @@ impl DataMgr {
             notes TEXT,
             zones BLOB,
             plant_type TEXT NOT NULL
-            )",
+            );",
                 NO_PARAMS,
             ).unwrap();
+        data.conn
+            .execute("CREATE TABLE IF NOT EXISTS crops (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                num_plants INTEGER NOT NULL,
+                date_planted TEXT NOT NULL,
+                plant_id INTEGER NOT NULL
+                FOREIGN KEY(plant_id) REFERENCES plants(id);", NO_PARAMS).unwrap();
         data
     }
 }

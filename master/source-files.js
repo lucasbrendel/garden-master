95796ec27964559416eaeb76b3ce935b21e00aa5
Travis CI User <travis@example.org>
2019-02-04 02:10:41 +0000

    Automatic Travis documentation build

--- master/source-files.js
+++ master/source-files.js
@@ -1,2 +1,3 @@
 var N = null;var sourcesIndex = {};
 sourcesIndex["green_thumb"] = {"name":"","dirs":[{"name":"db","dirs":[],"files":["crop.rs","mod.rs","plant.rs","task.rs"]}],"files":["logging.rs","main.rs"]};
+createSourceSidebar();

5d366ce6f094d518c2e99404569e0f0c1992c7ca
Travis CI User <travis@example.org>
2019-02-04 01:56:05 +0000

    Automatic Travis documentation build

--- master/implementors/serde/de/trait.Visitor.js
+++ master/implementors/serde/de/trait.Visitor.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["green_thumb"] = [{text:"impl&lt;'de&gt; <a class=\"trait\" href=\"https://docs.rs/serde/1.0.82/serde/de/trait.Visitor.html\" title=\"trait serde::de::Visitor\">Visitor</a>&lt;'de&gt; for __FieldVisitor",synthetic:false,types:[]},{text:"impl&lt;'de&gt; <a class=\"trait\" href=\"https://docs.rs/serde/1.0.82/serde/de/trait.Visitor.html\" title=\"trait serde::de::Visitor\">Visitor</a>&lt;'de&gt; for __Visitor&lt;'de&gt;",synthetic:false,types:[]},];
+implementors["green_thumb"] = [{text:"impl&lt;'de&gt; <a class=\"trait\" href=\"https://docs.rs/serde/1.0.85/serde/de/trait.Visitor.html\" title=\"trait serde::de::Visitor\">Visitor</a>&lt;'de&gt; for __FieldVisitor",synthetic:false,types:[]},{text:"impl&lt;'de&gt; <a class=\"trait\" href=\"https://docs.rs/serde/1.0.85/serde/de/trait.Visitor.html\" title=\"trait serde::de::Visitor\">Visitor</a>&lt;'de&gt; for __Visitor&lt;'de&gt;",synthetic:false,types:[]},];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
--- master/implementors/serde/ser/trait.Serialize.js
+++ master/implementors/serde/ser/trait.Serialize.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["green_thumb"] = [{text:"impl <a class=\"trait\" href=\"https://docs.rs/serde/1.0.82/serde/ser/trait.Serialize.html\" title=\"trait serde::ser::Serialize\">Serialize</a> for <a class=\"enum\" href=\"green_thumb/db/plant/enum.PlantType.html\" title=\"enum green_thumb::db::plant::PlantType\">PlantType</a>",synthetic:false,types:["green_thumb::db::plant::PlantType"]},];
+implementors["green_thumb"] = [{text:"impl <a class=\"trait\" href=\"https://docs.rs/serde/1.0.85/serde/ser/trait.Serialize.html\" title=\"trait serde::ser::Serialize\">Serialize</a> for <a class=\"enum\" href=\"green_thumb/db/plant/enum.PlantType.html\" title=\"enum green_thumb::db::plant::PlantType\">PlantType</a>",synthetic:false,types:["green_thumb::db::plant::PlantType"]},];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
